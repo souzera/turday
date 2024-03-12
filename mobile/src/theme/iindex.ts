2e1e7402bf0e95685e60497eb089b0b952dffa1d
matheus <ps.matheusb@gmail.com>
2024-03-11 23:31:55 -0300

    :lipstick: definindo constantes dos temas

---
+++ mobile/src/theme/iindex.ts
@@ -0,0 +1,20 @@
+export const THEME = {
+    COLORS: {
+        DARK: "#1F262A",
+        LIGHT: "#F8F8F8",
+        DANGER: "#FF4040",
+        WARNING: "#FFC413",
+        INFO: "#48D1CC",
+        SUCCESS: "#48D166",
+    },
+    FONT_FAMILY:{
+        REGULAR: "Montserrat-Regular",
+        BOLD: "Montserrat-Bold",
+        SEMI_BOLD: "Montserrat-SemiBold",
+    },
+    FONT_SIZE: {
+        SML: 14,
+        MD: 16,
+        LG: 24,
+    },
+}
