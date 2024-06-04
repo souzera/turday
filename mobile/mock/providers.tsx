e7a94d5a4291b08962aecbc935c8f1a22680397f
matheus <ps.matheusb@gmail.com>
2024-06-04 09:57:21 -0300

    :truck: move providers tree

---
+++ mobile/mock/providers.tsx
@@ -0,0 +1,13 @@
+import { LocationContextProvider } from "../src/context/location";
+
+export default function MockProviders({ children }: any) {
+
+    // STATES
+
+    // LIFECYCLE
+
+    // METHODS
+
+
+  return <LocationContextProvider>{children}</LocationContextProvider>;
+}
