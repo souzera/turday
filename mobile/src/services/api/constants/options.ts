2488d3f450115f3d402908e29ccd944ee1d31d5b
matheus <ps.matheusb@gmail.com>
2024-07-05 15:42:48 -0300

    :rocket: testando ngrok api tunel

--- mobile/src/services/api/constants/options.ts
+++ mobile/src/services/api/constants/options.ts
@@ -1,5 +1,13 @@
+/**
+ * 
 const options = {
     API_URL: `http://192.168.3.143:8000/api/`,
+}
+ */
+
+// ngrok api
+const options = {
+    API_URL: `https://dc2d-45-163-119-20.ngrok-free.app/api/`,
 }
 
 export default options;
